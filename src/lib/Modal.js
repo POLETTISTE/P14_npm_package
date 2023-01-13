68653aa9586e824991a5a3d5bd5781659026e5f3
POLETTI Stéphane <polettistephane@msn.com>
2023-01-13 12:07:47 +0100

    change props btn

--- src/lib/Modal.js
+++ src/lib/Modal.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Modal.css";
 
-export const Modal = ({ title, text, btnText, onClickFunction }) => (
+export const Modal = (props) => (
   <div className="modal-container">
     <div className="modal-window">
-      <h2>{title}</h2>
-      <p>{text}</p>
-      <button className="modal-btn" onclick={onClickFunction}>
-        {btnText}
+      <h2>{props.title}</h2>
+      <p>{props.text}</p>
+      <button
+        className="modal-btn"
+        data-id={props.id}
+        type={props.type}
+        name={props.name}
+        value={props.value}
+        disabled={props.disabled}
+        onclick={props.handleClick}>
+        {props.btnText}
       </button>
     </div>
   </div>
